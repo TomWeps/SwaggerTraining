f295b8bfe14ee3c2a5395e14c4b37462717af6db
Tomasz Weps <tweps@krolldiscovery.com>
2019-10-23 19:11:46 +0200

    added example data

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -7,27 +7,34 @@ namespace TodoApi.Models
     public class TodoItem
     {
         public long Id { get; set; }
-        
+
         /// <summary>
         /// Task's Name
         /// </summary>
+        /// <example>Conquer the world</example>
         [Required]
         public string Name { get; set; }
 
+        /// <example>Design an advanced control system that will allow you to control the world</example>
+        public string Description { get; set; }
 
+        /// <example>John Smith</example>
         public string AssignedPerson { get; set; }
 
         /// <summary>
         /// Flag is set, when task is done
         /// </summary>
+        /// <example>false</example>
         [DefaultValue(false)]
         public bool IsComplete { get; set; }
 
         /// <summary>
-        /// if the task has some time restriction, it has to be accomplished before some time.
+        /// time restriction - when the task has to be accomplished.
         /// </summary>
+        /// <example>2020-12-31T00:00:00.000Z</example>
         public DateTime? Deadline { get; set; }
-        
+
+        /// <example>Urgent</example>
         [DefaultValue(TodoPriority.Normal)]
         public TodoPriority Priority { get; set; }
 
